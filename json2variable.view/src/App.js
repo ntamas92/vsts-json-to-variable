1de268bde3d54fe97337b4911728876c5af3ff72
Tamás Nagy <wd975261@varian.com>
2021-03-30 14:39:35 +0200

    Add initial electron-based view

---
+++ json2variable.view/src/App.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import TreeView from '@material-ui/lab/TreeView';
+import TreeItem from '@material-ui/lab/TreeItem';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import './App.css';
+
+const ipcRenderer = window.ipcrenderer
+
+function App() {
+  const [folderContent, setFolderContent] = useState(null);
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  const [mergedContent, setMergedContent] = useState(null);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    const result = ipcRenderer.sendSync('select-dirs')
+    setFolderContent(result)
+  }
+
+  useEffect(() => {
+    if (!folderContent) {
+      setSelectedFiles([])
+    }
+  }, [folderContent])
+
+  const renderTree = (nodes) => {
+    return (<TreeItem key={nodes.path} nodeId={nodes.path} label={nodes.name}>
+      {Array.isArray(nodes.children) ? nodes.children.map((node) => renderTree(node)) : null}
+    </TreeItem>)
+  }
+
+  const handleJsonMerge = () => {
+    if (selectedFiles.length === 0) {
+      window.alert("Fukin select sum file already!")
+      return
+    }
+    
+    const result = ipcRenderer.sendSync('merge-json-files', selectedFiles.reverse())
+    setMergedContent(result)
+  }
+
+  return (
+    <div className="row">
+
+      <div className="left">
+        <p><button className="btn" type="button" className="btn btn-primary" onClick={handleClick} id="dirs">Choose a directory</button></p>
+
+        <div className="folder-view">
+          {folderContent &&
+            <div>Selected directory:
+          <TreeView className="directory-list"
+                defaultCollapseIcon={<ExpandMoreIcon />}
+                defaultExpanded={['root']}
+                defaultExpandIcon={<ChevronRightIcon />}
+                onNodeSelect={(_, nodeIds) => setSelectedFiles(nodeIds)}
+                multiSelect>
+                {renderTree(folderContent)}
+              </TreeView>
+            </div>}
+        </div>
+
+
+      </div>
+      <div className="right">
+        <div>
+          <button type="button" onClick={handleJsonMerge}>Merge files</button>
+        </div>
+        
+        <div className="merged-content">
+          {mergedContent && Object.keys(mergedContent).map(k => (<li><b>{k}</b>::{mergedContent[k]}</li>))}
+        </div>
+      </div>
+      
+    </div>
+
+  );
+}
+
+export default App;
